6cc59e698c5c6598a29fa75ecaba03144a129322
tony <linchiah01@gmail.com>
2022-10-26 16:11:18 +0800

    Initial Commit

---
+++ lin_bonus1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lin_bonus1
+//
+//  Created by User01 on 2022/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lin_bonus1/lin_bonus1App.swift
@@ -0,0 +1,17 @@
+//
+//  lin_bonus1App.swift
+//  lin_bonus1
+//
+//  Created by User01 on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct lin_bonus1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
